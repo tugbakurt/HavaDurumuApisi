18eebd5a3a12e68b98ba8b9a7b5b3b81181380a3
tugba kurt <tugba.basdogan@gmail.com>
2023-01-17 12:57:31 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace HavaDurumuApisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSehirAd = new System.Windows.Forms.Label();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSicaklik = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRuzgar = new System.Windows.Forms.Label();
+            this.lblRuz = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNem = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblYagis = new System.Windows.Forms.Label();
+            this.lblHissedilen = new System.Windows.Forms.Label();
+            this.lblHisSicaklik = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSehirAd
+            // 
+            this.lblSehirAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSehirAd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSehirAd.Location = new System.Drawing.Point(34, 59);
+            this.lblSehirAd.Name = "lblSehirAd";
+            this.lblSehirAd.Size = new System.Drawing.Size(240, 46);
+            this.lblSehirAd.TabIndex = 0;
+            this.lblSehirAd.Text = "İSTANBUL,TR";
+            // 
+            // lblTarih
+            // 
+            this.lblTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarih.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTarih.Location = new System.Drawing.Point(34, 130);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(240, 46);
+            this.lblTarih.TabIndex = 1;
+            this.lblTarih.Text = "İSTANBUL,TR";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(34, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bugün:";
+            // 
+            // lblSicaklik
+            // 
+            this.lblSicaklik.Font = new System.Drawing.Font("Microsoft Sans Serif", 48.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSicaklik.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSicaklik.Location = new System.Drawing.Point(338, 256);
+            this.lblSicaklik.Name = "lblSicaklik";
+            this.lblSicaklik.Size = new System.Drawing.Size(253, 119);
+            this.lblSicaklik.TabIndex = 3;
+            this.lblSicaklik.Text = "00.00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 268);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(573, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 46);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "o";
+            // 
+            // lblRuzgar
+            // 
+            this.lblRuzgar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRuzgar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblRuzgar.Location = new System.Drawing.Point(12, 413);
+            this.lblRuzgar.Name = "lblRuzgar";
+            this.lblRuzgar.Size = new System.Drawing.Size(111, 46);
+            this.lblRuzgar.TabIndex = 6;
+            this.lblRuzgar.Text = "Rüzgar:";
+            // 
+            // lblRuz
+            // 
+            this.lblRuz.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRuz.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblRuz.Location = new System.Drawing.Point(188, 413);
+            this.lblRuz.Name = "lblRuz";
+            this.lblRuz.Size = new System.Drawing.Size(111, 46);
+            this.lblRuz.TabIndex = 7;
+            this.lblRuz.Text = "Bugün:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(12, 472);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 46);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nem:";
+            // 
+            // lblNem
+            // 
+            this.lblNem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNem.Location = new System.Drawing.Point(188, 472);
+            this.lblNem.Name = "lblNem";
+            this.lblNem.Size = new System.Drawing.Size(111, 46);
+            this.lblNem.TabIndex = 9;
+            this.lblNem.Text = "Bugün:";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDurum.Location = new System.Drawing.Point(349, 352);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(240, 46);
+            this.lblDurum.TabIndex = 10;
+            this.lblDurum.Text = "Bugün:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(188, 525);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 46);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0 %";
+            // 
+            // lblYagis
+            // 
+            this.lblYagis.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYagis.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblYagis.Location = new System.Drawing.Point(12, 525);
+            this.lblYagis.Name = "lblYagis";
+            this.lblYagis.Size = new System.Drawing.Size(111, 46);
+            this.lblYagis.TabIndex = 11;
+            this.lblYagis.Text = "Yağış:";
+            // 
+            // lblHissedilen
+            // 
+            this.lblHissedilen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHissedilen.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHissedilen.Location = new System.Drawing.Point(188, 571);
+            this.lblHissedilen.Name = "lblHissedilen";
+            this.lblHissedilen.Size = new System.Drawing.Size(111, 46);
+            this.lblHissedilen.TabIndex = 14;
+            this.lblHissedilen.Text = "00.00";
+            // 
+            // lblHisSicaklik
+            // 
+            this.lblHisSicaklik.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHisSicaklik.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblHisSicaklik.Location = new System.Drawing.Point(12, 571);
+            this.lblHisSicaklik.Name = "lblHisSicaklik";
+            this.lblHisSicaklik.Size = new System.Drawing.Size(170, 46);
+            this.lblHisSicaklik.TabIndex = 13;
+            this.lblHisSicaklik.Text = "Sicaklik:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(768, 619);
+            this.Controls.Add(this.lblHissedilen);
+            this.Controls.Add(this.lblHisSicaklik);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblYagis);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblNem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRuz);
+            this.Controls.Add(this.lblRuzgar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblSicaklik);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTarih);
+            this.Controls.Add(this.lblSehirAd);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSehirAd;
+        private System.Windows.Forms.Label lblTarih;
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label lblSicaklik;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRuzgar;
+        private System.Windows.Forms.Label lblRuz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNem;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblYagis;
+        private System.Windows.Forms.Label lblHissedilen;
+        private System.Windows.Forms.Label lblHisSicaklik;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HavaDurumuApisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTarih.Text = DateTime.Now.ToShortDateString();
+            string api = "3cfb88a0a3c2dcf2b3be5b1177e86b44";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Konya&mode=xml&lang=tr&units=metric&appid=" +api;
+            XDocument hava = XDocument.Load(connection);
+            var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var ruzgar = hava.Descendants("speed").ElementAt(0).Attribute("value").Value;
+            var nem = hava.Descendants("humidity").ElementAt(0).Attribute("value").Value;
+            var durum = hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            lblSicaklik.Text = sicaklik.ToString();
+            lblRuz.Text = ruzgar +" m/s";
+            lblNem.Text = nem + " %";
+            lblDurum.Text = durum;
+            lblHissedilen.Text = sicaklik.ToString();
+            durum = "bulutlu";
+
+            if (durum=="açık")
+            {
+                pictureBox1.ImageLocation = @"C:\Users\tugba\Downloads\EnglishWords\sun_1_preview.jpg";
+            }
+            if (durum == "bulutlu")
+            {
+                pictureBox1.ImageLocation = @"C:\Users\tugba\Downloads\EnglishWords\bulutlu.png";
+            }
+        }
+    }
+}
+
+
